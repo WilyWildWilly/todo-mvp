e7f65e448cd71479f1218db5fe5e5726bf2af21f
guglielmoCG <guglielmo@atnos.com>
2020-08-24 13:20:59 +0200

    translated most of crystal.cr - except routing - and copied the template

--- todos/crystal/src/crystal.cr
+++ todos/crystal/src/crystal.cr
@@ -1,6 +1,55 @@
 # TODO: Write documentation for `Crystal`
-module Crystal
-  VERSION = "0.1.0"
+require "ecr"
 
-  # TODO: Put your code here
+print "I have started running!"
+
+class Todo
+  getter :name, :done, :id
+
+  def initialize(name, id)
+    print "I'm initializing a Todo!"
+    @name = name
+    @done = false
+    @id = id
+  end
+
+  def toggle
+    @done = !@done
+    self
+  end
 end
+
+class TodoList
+  getter :todos
+
+  ECR.def_to_s "./template.html.ecr"
+
+  def initialize
+    @todos = [] of Todo
+    @next_id = 0
+    print "I have initialized a TodoList!"
+  end
+
+  def add(name)
+    @todos.push(Todo.new(name, @next_id))
+    @next_id += 1
+  end
+
+  def delete(id)
+    @todos.delete_if { | todo | todo.id == id }
+  end
+
+  def toggle(id)
+    @todos = todos.map { | todo | todo.id == id ? todo.toggle : todo }
+  end
+
+  def render
+    @@template.result(binding)
+  end
+end
+
+# The todo 'API' - global (for now)
+# $todos = TodoList.new
+
+# Here's a simple top level router - could be a class if it implements 'call'
+
